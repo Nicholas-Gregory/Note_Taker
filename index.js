7f01ab8743ee9530828bff37625a2a92533792ef
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-07-19 13:01:49 -0700

    Added GET and POST routes and experimenting with unique id generators

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require("express");
+const uuid = require("uuid");
+
+const path = require("path");
+const fs = require("fs");
+
+const db = require("./db/db.json");
 
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 app.use(express.static("public"));
 app.use(express.json());
+
+
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
+
+app.get("/api/notes", (req, res) => res.json(db));
+
+app.post("/api/notes", (req, res) => {
+    let note = req.body;
+    note.id = uuid.v4();
+
+    db.push(note);
+
+    fs.writeFile("./db/db.json", JSON.stringify(db), "utf-8", (err) => err ? console.log(err) : console.log("Note written"));
+    res.send("Note written");
+});
+
+
+app.listen(PORT, () => console.log(`Note Taker App listening on port ${PORT}!`));
